eebb22a7b5d23bfcbeb1234c9c7cb73eb33ce1ae
Michal Ambroz <723625+xambroz@users.noreply.github.com>
2021-05-12 00:25:30 +0200

    add status images

--- copr_add_status_readme.sh
+++ copr_add_status_readme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find ./ -maxdepth 1 -type d | sort | cut -d / -f 2 | \
 while read I ; do 
-    echo "- $I - https://copr.fedorainfracloud.org/coprs/rebus/infosec/package/$I/status_image/last_build.png"
+    echo "- ![$I](https://copr.fedorainfracloud.org/coprs/rebus/infosec/package/$I/status_image/last_build.png) - $I"
 done >> README.md
 
 
