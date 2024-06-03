9dfb1f58a186e973ccc74e9e7de30cc5bb5c9af3
Michal Ambroz <723625+xambroz@users.noreply.github.com>
2024-06-03 16:33:36 +0200

    internal mainenance

--- github_update.sh
+++ github_update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+COPR=rebus/infosec
+CHROOTS=$( proxychains copr get rebus/infosec |grep download.copr| sed -e 's/^[ ]*//; s/:.*//' )
+PACKAGER=$( rpmdev-packager )
+CHDATE=$( LANG=en date "+%a %b %d %Y" )
+
 GITHUBCONF=~/.config/github.conf
 source $GITHUBCONF
 
@@ -10,7 +15,20 @@ if [ "$2" == "-f" ] ; then
 fi
 
 echo "=== Processing package $PACKAGE"
-SPECFILE="$PACKAGE/${PACKAGE}.spec"
+if [ -f "$PACKAGE/${PACKAGE}.spec" ] ; then
+	SPECFILE="$PACKAGE/${PACKAGE}.spec"
+elif [ -f "$PACKAGE/rawhide/${PACKAGE}.spec" ] ; then
+	SPECFILE="$PACKAGE/rawhide/${PACKAGE}.spec"
+elif [ -f "${PACKAGE}.spec" ] ; then
+	SPECFILE="${PACKAGE}.spec"
+elif [ -f "$PACKAGE" ] ; then
+	SPECFILE="$PACKAGE"
+        PACKAGE=$( basename "$PACKAGE" .spec )
+else
+	"=== ERROR: specfile for package $PACKAGE not found"
+	exit 1
+fi
+
 SPEC=$(cat "$SPECFILE" )
 GITNAME=$( echo "$SPEC" | grep "%global[ \t]*gitname" | awk '{print $3;}' )
 if [ $? -ne 0 -o "x${GITNAME}" = "x" ] ; then
@@ -20,6 +38,8 @@ if [ $? -ne 0 -o "x${GITNAME}" = "x" ] ; then
 fi
 
 GITUSER=$( echo "$SPEC" | grep "%global[ \t]*gituser" | awk '{print $3;}' )
+
+GITDATESPEC=$( echo "$SPEC" | grep "%global[ \t]*gitdate" | awk '{print $3;}' )
 
 if [ $? -ne 0 -o "x${GITUSER}" = "x" ] ; then
 	echo "ERROR: gituser not found in the spec file"
@@ -65,24 +85,43 @@ fi
 GITCOMMIT=$( echo "$HTML_RELEASE" | grep -o -P '(?<=\/commit\/)[a-f0-9]{40}(?=/hover)' | head -n 1 )
 SPECCOMMIT=$( echo "$SPEC" | grep -E '^%global[ \t]*commit[ \t]*[a-f0-9]{40}'| awk '{print $3}' | tail -n 1 )
 
-if [ "$GITVERSION" == "$SPECVERSION" -a "$GITCOMMIT" == "$SPECCOMMIT" ] ; then
-	echo "=== Package $PACKAGE is up to date: $GITVERSION , commit: $GITCOMMIT"
+JSON_COMMIT=$( curl --silent -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITAPIKEY" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/$GITUSER/$GITNAME/commits/$GITCOMMIT" )
+
+GITDATE=$( echo "$JSON_COMMIT" | jq ".commit.author.date" | sed -e 's/["-]//g; s/T.*$//;' )
+
+
+if [ "$GITVERSION" == "$SPECVERSION" -a "$GITCOMMIT" == "$SPECCOMMIT" -a "$GITDATE" == "$GITDATESPEC" ] ; then
+	echo "=== Package $PACKAGE is up to date: $GITVERSION , commit: $GITCOMMIT, gitdate: $GITDATE"
 	echo ""
 	exit 0
 fi
 
 echo "=== INFO: Package $PACKAGE needs update"
-echo "Git version:  $GITVERSION		Commit: $GITCOMMIT"
-echo "SPEC version: $SPECVERSION		Commit: $SPECCOMMIT"
+echo "Git version:  $GITVERSION		Commit: $GITCOMMIT	Gitdate: $GITDATE"
+echo "SPEC version: $SPECVERSION		Commit: $SPECCOMMIT	Gitdate: $GITDATESPEC"
 echo ""
 
 if [ "$FORCE" -eq 1 ] ; then
 	set -x
 	echo "=== Updating package $PACKAGE"
-	rpmdev-bumpspec --verbose --comment "bump to $GITVERSION" \
-		--new "$GITVERSION" "$SPECFILE"
-	sed -i -e "s/$SPECCOMMIT/$GITCOMMIT/;" "$SPECFILE"
-	git commit "$SPECFILE" -m "$PACKAGE - bump to $GITVERSION"
+
+	# WARNING - rpmdev-bumpspec doesn't hanlde %baserelease macro
+	# rpmdev-bumpspec --verbose --comment "bump to $GITVERSION" "$SPECFILE"
+
+	sed -i -e "s/\\(Version:.*\\)${SPECVERSION}/\\1${GITVERSION}/;
+	           s/\\(^%global[ \t]*baserelease[ \t]*\\)[0-9]*/\\11/;
+                   s/\\(^%global[ \t]*commit[ \t]*\\)${SPECCOMMIT}/\\1${GITCOMMIT}/;
+                   s/\\(^%global[ \t]*gitdate[ \t]*\\)${GITDATESPEC}/\\1${GITDATE}/;
+                   s/\\(^%changelog\\)/\\1\\n\* $CHDATE $PACKAGER - ${GITVERSION}-1\n- bump to $GITVERSION\\n/;
+        " "$SPECFILE"
+
+
+	git diff ./
+	echo git commit "$SPECFILE" -m "$PACKAGE - bump to $GITVERSION"
 fi
 
 
--- rebuild_libesedb.sh
+++ rebuild_libesedb.sh
@@ -1,223 +1,38 @@
 #!/bin/bash
 
-SEQUENCE1="
-libcstring
-libcerror
-libcthreads
-libcdata
-libcdatetime
-libclocale
-libcnotify
-libcsplit
-"
+SEQUENCEFILE="sequence.txt"
+SEQUENCE=$(cat "$SEQUENCEFILE" | sed -e 's/[ ]*#.*$//' | grep -v -e '^$')
 
-SEQUENCE2="
-libuna
-libcfile
-libcpath
-libbfio
-libfcache
-libfdata
-libfvalue
-libfguid
-libfdatetime
-libfwnt
-libhmac
-libcaes
-libodraw
-libsmdev
-libsmraw
-libcsystem
-libfmapi
-libmapidb
-"
+LASTGOODFILE="last_good.txt"
+LASTGOOD=$(cat "$LASTGOODFILE")
+SKIP=1
+if [ "x${LASTGOOD}" == "x" ] ; then
+    SKIP=0
+fi
 
-for I in $SEQUENCE ; do
-    copr build-package rebus/infosec --name "$I"
-    RESULT=$!
+echo "$SEQUENCE" | while read P DEPARGS ; do
+    if [ $SKIP -eq 1 -a "$LASTGOOD" != "$P" ] ; then
+        echo "=== Skipping: $P"
+        continue
+    elif [ "$LASTGOOD" == "$P" ] ; then
+        echo "=== Skipping: $P"
+        SKIP=0
+        continue
+    fi
+
+
+    DEPS=$( echo "$DEPARGS" | cut -d '|' -f 1 )
+    ARGS=$( echo "$DEPARGS" | cut -d '|' -f 2 )
+
+    echo "=== Building package $P"
+    echo "copr build-package rebus/infosec --name \"$P\" $ARGS"
+    MSG=$( proxychains copr build-package rebus/infosec --name "$P" $ARGS )
+    RESULT=$?
     if [ "$RESULT" -ne 0 ] ; then
-        echo "==== ERROR: failed package $I "
+        echo "==== ERROR: failed package $P "
         exit
+    else
+        echo "==== SUCCESS building package $P"
+        echo "$P" > $LASTGOODFILE
     fi
 done
-
-# libcstring	DONE
-# libcerror	DONE
-
-# libcthreads
-#	libcerror
-
-# libcdata	DONE
-#        libcerror \
-#        libcthreads \
-
-# libcdatetime	DONE
-#        libcerror \
-
-
-# libclocale	DONE
-#        libcerror \
-
-# libcnotify	DONE
-#        libcerror \
-
-
-# libcsplit	DONE
-#        libcerror \
-
-# libuna		PARTIAL !!!!!!!!
-#        libcstring \
-#        libcerror \
-#        libcdatetime \
-#        libclocale \
-#        libcnotify \
-#        libcfile \
-#        libcsystem \
-
-
-# libcfile	DONE
-#	libclocale \
-#        libcnotify \
-#        libuna \
-
-# libcpath	DONE
-#        libcstring \
-#        libcerror \
-#        libclocale \
-#        libcsplit \
-#        libuna \
-
-
-# libbfio		DONE
-#        libcstring \
-#        libcerror \
-#        libcthreads \
-#        libcdata \
-#        libclocale \
-#        libcnotify \
-#        libcsplit \
-#        libuna \
-#        libcfile \
-#        libcpath \
-
-
-# libfcache	DONE
-#        libcstring \
-#        libcerror \
-#        libcthreads \
-#        libcdata \
-
-
-# libfdata	DONE
-#        libcstring \
-#        libcerror \
-#        libcthreads \
-#        libcdata \
-#        libcnotify \
-#        libfcache \
-
-
-# libfvalue	DONE
-#        libcstring \
-#        libcerror \
-#        libcthreads \
-#        libcdata \
-#        libcnotify \
-#        libuna \
-#        libfdatetime \
-#        libfguid \
-#        libfwnt \
-
-
-# libfguid	DONE
-#        libcstring \
-#        libcerror \
-
-# libfdatetime	DONE
-#        libcstring \
-#        libcerror \
-
-
-# libfwnt		DONE
-#        libcstring \
-#        libcerror \
-#        libcthreads \
-#        libcdata \
-#        libcnotify \
-
-
-# libhmac		DONE
-#        libcstring \
-#        libcerror \
-#        libclocale \
-#        libcnotify \
-#        libcsplit \
-#        libuna \
-#        libcfile \
-#        libcpath \
-#        libcsystem \
-
-
-# libcaes		DONE
-#        libcstring \
-#        libcerror \
-
-
-# libodraw	DONE
-#        libcstring \
-#        libcerror \
-#        libcthreads \
-#        libcdata \
-#        libclocale \
-#        libcnotify \
-#        libcsplit \
-#        libuna \
-#        libcfile \
-#        libcpath \
-#        libbfio \
-#        libcsystem \
-#        libhmac \
-
-
-# libsmdev	DONE
-#        libcstring \
-#        libcerror \
-#        libcthreads \
-#        libcdata \
-#        libclocale \
-#        libcnotify \
-#        libuna \
-#        libcfile \
-#        libcsystem \
-
-
-# libsmraw	DONE
-#        libcstring \
-#        libcerror \
-#        libcthreads \
-#        libcdata \
-#        libclocale \
-#        libcnotify \
-#        libcsplit \
-#        libuna \
-#        libcfile \
-#        libcpath \
-#        libbfio \
-#        libfcache \
-#        libfdata \
-#        libfvalue \
-#        libcsystem \
-#        libhmac \
-
-
-# libcsystem	DONE
-#        libcstring \
-#        libcerror \
-#        libclocale \
-#        libcnotify \
-#        libuna \
-
-
-# libfmapi	DONE
-
-# libmapidb	DONE
